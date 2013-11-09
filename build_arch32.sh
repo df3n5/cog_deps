5b4bef3e3af49058d898f1c99536ddc2c9651a04
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-11-09 23:08:57 +0000

    Adding SDL2

--- build_arch32.sh
+++ build_arch32.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 #Create directories
 mkdir -p arch32/{include,lib}
@@ -6,18 +7,22 @@ dest_dir=`pwd`
 popd
 
 #libpng
-echo "Building libpng..."
-pushd src/libpng
-./configure --prefix=$dest_dir
-make
-make DESTDIR=$dest_dir install
-popd
-echo "Built libpng..."
-
-#SDL2
-#pushd src/SDL2
-#./autogen.sh
+#echo "Building libpng..."
+#pushd src/libpng
 #./configure --prefix=$dest_dir
 #make
 #make DESTDIR=$dest_dir install
 #popd
+#echo "Built libpng..."
+
+#SDL2
+build_sdl () {
+    pushd src/SDL2
+    ./autogen.sh
+    ./configure --prefix=$dest_dir
+    make
+    make install
+    popd
+}
+
+build_sdl
