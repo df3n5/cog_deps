be387ef868cdeedd75789b60f1ba716511e9bed9
Jonathan Frawley <jonathanfrawley@gmail.com>
2013-11-09 22:54:32 +0000

    Removing SDL2 folder

--- build_arch32.sh
+++ build_arch32.sh
@@ -5,10 +5,19 @@ pushd arch32
 dest_dir=`pwd`
 popd
 
-#SDL2
-pushd src/SDL2
-./autogen.sh
+#libpng
+echo "Building libpng..."
+pushd src/libpng
 ./configure --prefix=$dest_dir
 make
 make DESTDIR=$dest_dir install
 popd
+echo "Built libpng..."
+
+#SDL2
+#pushd src/SDL2
+#./autogen.sh
+#./configure --prefix=$dest_dir
+#make
+#make DESTDIR=$dest_dir install
+#popd
